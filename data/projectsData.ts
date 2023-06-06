9a54739819ea8a174a0d1faae3e063e8ccda6edc
Cristian Romo <c.romo98@gmail.com>
2023-06-05 21:20:40 -0700

    init commit

---
+++ data/projectsData.ts
@@ -0,0 +1,11 @@
+const projectsData = [
+  {
+    title: 'ITSM: How to Automate IT Provisioning',
+    description: `My first post as a Product Marketing Associate shows an overview of an integration
+    using our platform, Celigo, and highlights how to integrate a common business process.`,
+    imgSrc: '/static/images/celigo.png',
+    href: 'https://www.celigo.com/blog/itsm-it-provisioning/',
+  },
+]
+
+export default projectsData
